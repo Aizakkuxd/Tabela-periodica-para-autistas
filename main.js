94e0d3cea5bfe31cb1247a5c29c9d15d4fe1b42c
Aizakkuxd <113270152+Aizakkuxd@users.noreply.github.com>
2023-01-28 20:28:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+interactive('element0');
+
+// NO resizable
+interactive('element2', { resize: false } );
+
+// NO close function
+let options =  {
+  close: false
+};
+interactive('element1', options);
